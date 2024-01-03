c3361820d30c386ccff58d0224276b11ebe1e6c3
Kevin3874 <kzhang77@jh.edu>
2024-01-03 16:22:47 -0500

    Added test api

--- techstack-backend/routes/index.js
+++ techstack-backend/routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const scrapeRouter = require('./scrape.js');
+const testRouter = require('./test.js');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('Welcome to the TechStack API!');
+});
 router.use('/scrape', scrapeRouter); 
+router.use('/test', testRouter);
 
 module.exports = router;
