a542c3530168af791faea6935b43d15561003a1e
Kevin3874 <kzhang77@jh.edu>
2024-01-04 01:09:48 -0500

    Connection: keep-alive

--- techstack-backend/service/scrapeService.js
+++ techstack-backend/service/scrapeService.js
@@ -18,6 +18,7 @@ async function scrapeWithPuppeteer(browser, scrapeFunction, url) {
   await page.setExtraHTTPHeaders({
     'Accept-Language': 'en-US,en;q=0.9',
     'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
   });
   await page.setRequestInterception(true);
   page.on('request', (req) => {
